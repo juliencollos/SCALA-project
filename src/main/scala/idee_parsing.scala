65d58f651a42ed1ef136bb136fe0c0c0326cc63c
Julien Collos <julien.collos@epita.fr>
2020-05-29 11:36:31 +0200

    correction line

--- src/main/scala/idee_parsing.scala
+++ src/main/scala/idee_parsing.scala
@@ -2,13 +2,13 @@
 
 def parseFile(line: String): Option[Data] = {
     line.split(";").toList match {
-        case List(nos data) => Some(Data(nos data))
+        case List(nos data) => Some(Data(nos data)) //nos data = (id, ident, etc..)
         case _ => None
     }
 }
 
 def readDataAirport(fileName: String): List[Data] = {
     val filename = "airport.csv"
-    val lines = Source.fromFile(filename).geetLines.toList
+    val lines = Source.fromFile(filename).getLines.toList
     val data = parseFile(lines)
 }
